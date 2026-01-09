2fdd0b2acea4c0f79df30fe6ff1c6cbe9f54f448
Satish Pokharel <satishpokharel99@gmail.com>
2026-01-09 14:04:05 +0545

    added another way to connect db in seprate file for future ref

--- src/index.js
+++ src/index.js
@@ -5,25 +5,3 @@ dotenv.config()
 console.log("MONGO URI:", process.env.MONGODB_URI);
 
 connectDB();
-/*
-import express from 'express'
-
-const app = express()
-
-;(async () => {
-    try{
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-        app.on("Error", (error) => {
-            console.log("ERRR:", error);
-            throw error
-        })
-
-        app.listen(process.env.PORT, () => {
-            console.log(`App is listening on port ${process.env.PORT}`);
-        })
-
-    } catch (error){
-        console.error("ERROR", error)
-        throw err
-    }
-})()*/
